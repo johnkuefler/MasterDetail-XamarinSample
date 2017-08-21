7ff48bce8316f94bdaddd77b2aafd7e2f73afe6e
jkuefler <john.kuefler@hotmail.com>
2017-08-20 22:39:12 -0500

    Add project files.

---
+++ SampleMasterDetail/SampleMasterDetail/Models/RootPageMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMasterDetail.Models
+{
+    public class RootPageMenuItem
+    {
+        public string Title { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
---
+++ SampleMasterDetail/SampleMasterDetail/Views/RootPageMaster.xaml.cs
@@ -0,0 +1,44 @@
+﻿using SampleMasterDetail.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleMasterDetail.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RootPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public RootPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new RootPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class RootPageMasterViewModel
+        {
+            public ObservableCollection<RootPageMenuItem> MenuItems { get; set; }
+
+            public RootPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<RootPageMenuItem>(new[]
+                {
+                    new RootPageMenuItem { Title = "Page 1", TargetType= typeof(Page1) },
+                    new RootPageMenuItem { Title = "Page 2", TargetType= typeof(Page2) },
+                    new RootPageMenuItem { Title = "Page 3", TargetType= typeof(Page3) },
+                });
+            }
+        }
+    }
+}
